5e8772fe58952c54562936b2ab086ee72b3756cc
Anshitha-27 <158013129+Anshitha-27@users.noreply.github.com>
2025-02-21 14:37:07 +0530

    Add files via upload

---
+++ t1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // Declare an integer variable
+    let integer_var: i32 = 10;
+    
+    // Declare a floating-point variable
+    let float_var: f64 = 3.14;
+    
+    // Declare a boolean variable
+    let bool_var: bool = true;
+    
+    // Declare a character variable
+    let char_var: char = 'A';
+    
+    // Print the values of each variable
+    println!("Integer: {}", integer_var);
+    println!("Float: {}", float_var);
+    println!("Boolean: {}", bool_var);
+    println!("Character: {}", char_var);
+}
+
